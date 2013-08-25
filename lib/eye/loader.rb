1640709ac3c78062aa9c026c5acfb059315f166d
Konstantin Makarchev <kostya27@gmail.com>
2013-08-25 20:28:43 +0400

    upd celluloid to 0.15

--- lib/eye/loader.rb
+++ lib/eye/loader.rb
@@ -1,7 +1,7 @@
 # mini bundler, for embedded server gem installation
 
-gem 'celluloid',     '~> 0.14.0'
-gem 'celluloid-io',  '~> 0.14.0'
+gem 'celluloid',     '~> 0.15.0.pre'
+gem 'celluloid-io',  '~> 0.15.0.pre'
 gem 'nio4r'
 gem 'timers'
 
